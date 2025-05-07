fa803e22a73f29c619f8fa48801569f0895c3241
NicoMico <Administrator@DESKTOP>
2025-05-07 11:18:11 +0800

    op

--- config/main_config.py
+++ config/main_config.py
@@ -25,7 +25,7 @@ class GlobalConfig:
         if cls.gamename in ["GI","HI3","ZZZ","BloodySpell","GF2","IdentityV"]:
             return GameCategory.UnityVS
         
-        elif cls.gamename in ["Game001","HSR","AILIMIT"]:
+        elif cls.gamename in ["Game001","AILIMIT"]:
             return GameCategory.UnityCS
         
         elif cls.gamename in ["Game002","WWMI"]:
