157d97d030d304a4c2f896e62caef25bb10d900f
NicoMico <Administrator@DESKTOP>
2025-05-27 10:24:27 +0800

    修复更新面板无法显示的BUG

--- __init__.py
+++ __init__.py
@@ -87,6 +87,51 @@ class UpdaterPanel(bpy.types.Panel):
         # addon_updater_ops.update_notice_box_ui(self, context)
         addon_updater_ops.update_settings_ui(self, context)
 
+
+
+
+class HertaUpdatePreference(bpy.types.AddonPreferences):
+    # Addon updater preferences.
+    bl_idname = __package__
+
+
+    auto_check_update: bpy.props.BoolProperty(
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=True) # type: ignore
+
+    updater_interval_months: bpy.props.IntProperty(
+        name='Months',
+        description="Number of months between checking for updates",
+        default=0,
+        min=0) # type: ignore
+
+    updater_interval_days: bpy.props.IntProperty(
+        name='Days',
+        description="Number of days between checking for updates",
+        default=1,
+        min=0,
+        max=31) # type: ignore
+
+    updater_interval_hours: bpy.props.IntProperty(
+        name='Hours',
+        description="Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23) # type: ignore
+
+    updater_interval_minutes: bpy.props.IntProperty(
+        name='Minutes',
+        description="Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59) # type: ignore
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "auto_check_update")
+        addon_updater_ops.update_settings_ui(self, context)
+
+
 register_classes = (
     # 全局配置
     Properties_ImportModel,
@@ -143,57 +188,17 @@ register_classes = (
     PanelModelProcess,
 
 
+    HertaUpdatePreference,
 
+    
     ExtractSubmeshOperator,
     PanelModelSplit,
+
 
     # SSMT预备代码
     # PanelSSMTExtractModel,
     # SSMTExtractModelGI,
 )
-
-
-
-class HertaUpdatePreference(bpy.types.AddonPreferences):
-    # Addon updater preferences.
-    bl_idname = __package__
-
-
-    auto_check_update: bpy.props.BoolProperty(
-        name="Auto-check for Update",
-        description="If enabled, auto-check for updates using an interval",
-        default=True) # type: ignore
-
-    updater_interval_months: bpy.props.IntProperty(
-        name='Months',
-        description="Number of months between checking for updates",
-        default=0,
-        min=0) # type: ignore
-
-    updater_interval_days: bpy.props.IntProperty(
-        name='Days',
-        description="Number of days between checking for updates",
-        default=1,
-        min=0,
-        max=31) # type: ignore
-
-    updater_interval_hours: bpy.props.IntProperty(
-        name='Hours',
-        description="Number of hours between checking for updates",
-        default=0,
-        min=0,
-        max=23) # type: ignore
-
-    updater_interval_minutes: bpy.props.IntProperty(
-        name='Minutes',
-        description="Number of minutes between checking for updates",
-        default=0,
-        min=0,
-        max=59) # type: ignore
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, "auto_check_update")
-        addon_updater_ops.update_settings_ui(self, context)
 
 
 def register():
